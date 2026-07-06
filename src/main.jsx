6cb05b780a5911740a937de3d671fcc304d933c1
최보미 <bomi@choebomiui-MacBookPro.local>
2026-07-07 02:04:32 +0900

    music player end

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import Day02 from "./pages/day02/Day02";
 import Day03 from "./pages/day03/Day03";
 import "./style/index.css";
-import MusicUiEx from "./pages/musicUi/MusicUiEx";
 import Day04 from "./pages/day04/Day04";
+import MusicUiEx from "./pages/musicUi/MusicUiEx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <Day02 /> */}
     {/* <Day02 /> */}
     {/* <Day03 /> */}
-    {/* <MusicUiEx /> */}
-    <Day04 />
+    {/* <Day04 /> */}
+    <MusicUiEx />
   </StrictMode>,
 );
--- src/pages/musicUi/components/Title.jsx
+++
@@ -1,7 +0,0 @@
-export default function Title() {
-  return (
-    <div>
-      <div>{/* 앨범표지 */}</div>
-    </div>
-  );
-}
